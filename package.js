15725e25bd954af54605c0b1f7a8e6eee8548a18
Dominik Ferber <dominik.ferber@gmail.com>
2015-08-31 23:35:44 +0200

    release v2.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'dferber:reactive-var-toggle',
-	version: '1.0.2',
+	version: '2.0.0',
 	summary: 'Toggle boolean values of reactive variables.',
 	git: 'https://github.com/dferber90/meteor-reactive-var-toggle',
 	documentation: 'README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
 	'use strict';
-	
+
 	api.versionsFrom('1.0');
 	api.use('reactive-var');
 	api.addFiles('toggle.js', 'client');
